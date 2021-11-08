4c13754e8321f879afb465d31a979f2c19b115ae
t-okkn <okkn@okkn.net>
2021-11-08 23:41:03 +0900

    [feat] QuestionSetにサブネットマスクを挿入できるように修正

--- models/exchange.go
+++ models/exchange.go
@@ -1,11 +1,12 @@
 package models
 
 type QuestionSet struct {
-	Id       string `json:"id"`
-	Number   int    `json:"number"`
-	Source   string `json:"source"`
-	CIDRbits int    `json:"cidr_bits"`
-	IsCIDR   int    `json:"is_cidr"`
+	Id         string `json:"id"`
+	Number     int    `json:"number"`
+	Source     string `json:"source"`
+	CIDRbits   int    `json:"cidr_bits"`
+	IsCIDR     int    `json:"is_cidr"`
+	SubnetMask string `json:"subnet_mask"`
 }
 
 type AnswerSet struct {
