9406741c67dbc05a24d18b233528e2a729f95e3a
t-okkn <okkn@okkn.net>
2021-11-16 00:29:43 +0900

    [fix] AnswerSetをformのBind用構造体に修正

--- models/exchange.go
+++ models/exchange.go
@@ -10,10 +10,16 @@ type QuestionSet struct {
 }
 
 type AnswerSet struct {
-	Id      string `json:"id"`
-	Number  int    `json:"number"`
-	NwAddr  string `json:"answer_nw"`
-	BcAddr  string `json:"answer_bc"`
-	Elapsed int    `json:"elapsed"`
+	Id         string `form:"id"`
+	Number     string `form:"number"`
+	NwAddr1st  string `form:"nwaddr_1st"`
+	NwAddr2nd  string `form:"nwaddr_2nd"`
+	NwAddr3rd  string `form:"nwaddr_3rd"`
+	NwAddr4th  string `form:"nwaddr_4th"`
+	BcAddr1st  string `form:"bcaddr_1st"`
+	BcAddr2nd  string `form:"bcaddr_2nd"`
+	BcAddr3rd  string `form:"bcaddr_3rd"`
+	BcAddr4th  string `form:"bcaddr_4th"`
+	Elapsed    string `form:"elapsed"`
 }
 
