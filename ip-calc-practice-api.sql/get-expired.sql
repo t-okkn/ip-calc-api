527bafa407154e85aa8752338b409b69a4324b92
t-okkn <okkn@okkn.net>
2021-11-28 20:42:26 +0900

    [fix] expireの型が変わってしまっていた問題を修正

--- ip-calc-practice-api.sql/get-expired.sql
+++ ip-calc-practice-api.sql/get-expired.sql
@@ -2,6 +2,6 @@ SELECT
   `id`,
   `total`,
   CAST(`is_end` AS UNSIGNED) AS `is_end`,
-  STR_TO_DATE(`expire`, '%Y-%m-%dT%H:%i:%s') AS `expire`
+  `expire`
 FROM T_ID
-WHERE `expire` < NOW();
+WHERE STR_TO_DATE(`expire`, '%Y-%m-%dT%H:%i:%s') < NOW();
